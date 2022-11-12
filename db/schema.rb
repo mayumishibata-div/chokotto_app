125df2918edbee27b4de21209ad70d9292e338db
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2022-11-12 15:31:31 +0900

    Revert "room_usersテーブルの作成"
    This reverts commit 34b10b89d969b39d271036e329c3d78adb9df99d.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_10_121247) do
+ActiveRecord::Schema.define(version: 2022_11_12_062034) do
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
